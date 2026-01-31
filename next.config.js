106379ee36cc56b2cca0c9894d5ada1ff55da144
IceFerryLing <523918392@qq.com>
2026-01-31 21:02:18 +0800

    next

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: ["mongoose"],
+
   experimental: {
-    serverActions: true, // 实验性服务器
-    serverComponentsExternalPackages: ["mongoose"],
+    serverActions: {allowedOrigins: ["*"]}, // 实验性服务器
+    // serverComponentsExternalPackages: ["mongoose"],
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  // eslint: {
+  //   // Warning: This allows production builds to successfully complete even if
+  //   // your project has ESLint errors.
+  //   ignoreDuringBuilds: true,
+  // },
   images: {
     remotePatterns: [
       {
