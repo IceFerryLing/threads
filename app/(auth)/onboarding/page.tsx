4d39c7b68db387c5d1c5964dc50e9fecd6f31e95
IceFerryLing <523918392@qq.com>
2026-02-01 00:24:46 +0800

    页面

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -9,7 +9,7 @@ async function Page() {
   }
   
   const userData = {
-    id: 'WILL COME FROM DATABASE...',
+    id: user?.id || '',
     objectId: userInfo?._id,
     username: userInfo?.username || user?.username,
     name: userInfo?.name || user?.firstName || "",
