b43f1da05a65b1469dc377aaff03bdb7ce545cbf
IceFerryLing <523918392@qq.com>
2026-02-06 10:57:42 +0800

    加入注释

--- lib/models/thread.model.ts
+++ lib/models/thread.model.ts
@@ -1,29 +1,36 @@
 import mongoose from "mongoose";
 
+// Schema函数定义了Thread模型的结构
 const threadSchema = new mongoose.Schema({
+  // 主要内容
   text: {
     type: String,
     required: true,
   },
+  // 作者引用
   author: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true,
   },
+  // 所属社区引用
   community: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Community",
   },
+  // 创建时间
   createdAt: {
     type: Date,
     default: Date.now,
   },
+  // 父线程引用（如果有的话）
   parentId: {
     type: String,
   },
+  // 子线程引用数组
   children: [
     {
-      type: mongoose.Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId, 
       ref: "Thread",
     },
   ],
