1808924fa3d003f3a7655cb7d473b2da7a5692df
IceFerryLing <523918392@qq.com>
2026-02-06 10:58:03 +0800

    CommentValidation

--- lib/validations/thread.ts
+++ lib/validations/thread.ts
@@ -5,3 +5,7 @@ export const ThreadValidation = z.object({
   thread: z.string().nonempty().min(1).max(5000),
   accoutId: z.string(),
 })
+
+export const CommentValidation = z.object({
+  thread: z.string().nonempty().min(1).max(5000),
+})
