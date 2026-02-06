dcd5995a021c30a99daf5bc2801a17067750c3a6
IceFerryLing <523918392@qq.com>
2026-02-06 15:02:28 +0800

    更改

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,11 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import { title } from "process";
-
+import React from "react";
+import type { Metadata } from "next";
+import { dark } from "@clerk/themes";
 import '../globals.css';
+
 // 定义页面的元数据
 export const metadata = {
     title: 'Threads',
@@ -19,7 +22,11 @@ const inter = Inter({ subsets: ['latin'] })
  */
 export default function RootLayout({ children }:{ children: React.ReactNode }){
     return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
         <html lang="en">
             <body className={`${inter.className} bg-dark-1`}>
                 {children}
